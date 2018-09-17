5d44b0591eb60d593a96018633cf4194f89e67f8
Ashley Schultz <ashleyeschultz@gmail.com>
2018-09-17 18:11:20 +0000

    Update cors file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
-  resources :game, only: [:index, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :game do
+    resources :games, only: [:index, :create]
+  end
 end
