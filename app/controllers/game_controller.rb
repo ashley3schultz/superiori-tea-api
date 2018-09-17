81523a84c7fcee45c7cbea7c68dda62bd7d73d52
Ashley Schultz <ashleyeschultz@gmail.com>
2018-09-17 18:06:43 +0000

    Add game model

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,8 +1,19 @@
 class GameController < ApplicationController
 
   def index
-    @games = Game.all
-    render json: @games
+    @game = Game.order('score desc').first
+    render json: @game
   end
+
+  def create
+    @game = Game.create(game_params)
+    render json: @game
+  end
+
+  private
+
+    def game_params
+      params.reuire(:game).permit(:user, :score)
+    end
 
 end
