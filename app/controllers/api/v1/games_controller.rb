a3eb51adf8dffcc94b079f86f7999bce4a9085d4
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-17 21:54:19 -0700

    Resctructure game controller into nested  API folder

---
+++ app/controllers/api/v1/games_controller.rb
@@ -0,0 +1,18 @@
+module Api::V1
+    class GamesController < ApplicationController
+
+        def index 
+            @game = Game.order('score DESC').first
+            render json: @game
+        end
+
+        def create
+            user = User.find_or_create_by(name: params[:name])
+            @game = Game.create(score: params[:score], user_id: user.id)
+            render json: @game
+        end 
+    end
+end
+
+
+# rails s -p 3001
