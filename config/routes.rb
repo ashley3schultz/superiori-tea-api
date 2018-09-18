76e07c86dbf0bc91a5affaf62778a879681f2231
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-17 21:08:16 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+
     resources :games, only: [:index, :create]
-  end
-
-  resources :games, only: [:index, :create]
-
+    # resources :users, only: [:show]
 end
