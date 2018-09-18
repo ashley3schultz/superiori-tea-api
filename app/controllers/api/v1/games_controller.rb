ad7ea4500166ad1c63c5c969082265d68727de4c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-09-18 20:16:40 +0000

    Update create action for game

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,18 +1,20 @@
 module Api::V1
     class GamesController < ApplicationController
 
-        def index 
-            @game = Game.order('score DESC').first
+        def index
+            game = Game.order('score DESC').first
+            @game = {score: game.score, name: game.user.name}
             render json: @game
         end
 
         def create
-            user = User.find_or_create_by(name: params[:name])
-            @game = Game.create(score: params[:score], user_id: user.id)
+            user = User.find_or_create_by(name: params[:game][:name])
+            new_game = Game.create(score: params[:game][:score], user_id: user.id)
+            game = Game.order('score DESC').first
+            @game = {score: game.score, name: game.user.name}
             render json: @game
-        end 
+        end
     end
 end
-
 
 # rails s -p 3001
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins '192.168.1.30:3000'
 
     resource '*',
       headers: :any,
