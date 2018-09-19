a079de068c5f88b6ad2f85a69af426c8f4cb0e6f
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-19 15:01:54 -0700

    Remove user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_034635) do
+ActiveRecord::Schema.define(version: 2018_09_18_034622) do
 
   create_table "games", force: :cascade do |t|
-    t.integer "user_id"
+    t.string "user"
     t.integer "score"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
