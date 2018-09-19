ab4a9ca3c882198f6a3063c68ae7c792754247de
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-19 15:02:16 -0700

    Update to fetch all games

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,16 +2,13 @@ module Api::V1
     class GamesController < ApplicationController
 
         def index
-            game = Game.order('score DESC').first
-            @game = {score: game.score, name: game.user.name}
-            render json: @game
+            @games = Game.order('score DESC')
+            render json: @games
         end
 
         def create
             user = User.find_or_create_by(name: params[:game][:name])
-            new_game = Game.create(score: params[:game][:score], user_id: user.id)
-            game = Game.order('score DESC').first
-            @game = {score: game.score, name: game.user.name}
+            @game = Game.create(score: params[:game][:score], user_id: user.id)
             render json: @game
         end
     end
