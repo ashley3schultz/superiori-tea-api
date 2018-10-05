87b36012493ad2fc19980f5388189b221c5f6327
ashley3schultz <ashleyeschultz@gmail.com>
2018-10-04 18:55:09 -0600

    Update fetching urls allowed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'superiori-tea.herokuapp'
+    origins 'https://superiori-tea.herokuapp'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
