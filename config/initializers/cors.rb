db1a61e7cad7a9ef60b7c6b0cea85e974a399997
ashley3schultz <ashleyeschultz@gmail.com>
2018-10-04 21:24:38 -0600

    Update url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://superiori-tea.herokuapp.com/', 'http://localhost:3000/']
+    origins ['superiori-tea.herokuapp', 'localhost:3000']
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
